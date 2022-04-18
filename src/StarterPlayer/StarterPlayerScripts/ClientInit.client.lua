5b7a58c76464a12a791828413248379d97adfdf8
LuaNathanCarr <102487915+LuaNathanCarr@users.noreply.github.com>
2022-04-17 21:21:32 -0500

    Update ClientInit.client.lua
    fix emma's broken shit

--- src/StarterPlayer/StarterPlayerScripts/ClientInit.client.lua
+++ src/StarterPlayer/StarterPlayerScripts/ClientInit.client.lua
@@ -1,16 +1,16 @@
-local Players = game:GetService("Players")
---local StarterGui = game:GetService("StarterGui")
+-- local Players = game:GetService("Players")
+-- --local StarterGui = game:GetService("StarterGui")
 
-local LocalPlayer = Players.LocalPlayer
+-- local LocalPlayer = Players.LocalPlayer
 
---Dependency group 0
-local managersFolder = LocalPlayer.PlayerScripts
+-- --Dependency group 0
+-- local managersFolder = LocalPlayer.PlayerScripts
 
---Dependency group 1
-local uiFolder = managersFolder.UI
-local platformDetectionManager = require(managersFolder:WaitForChild("PlatformDetectionManager"))
+-- --Dependency group 1
+-- local uiFolder = managersFolder.UI
+-- local platformDetectionManager = require(managersFolder:WaitForChild("PlatformDetectionManager"))
 
---Dependency group 2
-local hudUIManager = require(uiFolder:WaitForChild("HUDUIManager"))
-hudUIManager.new(platformDetectionManager)
+-- --Dependency group 2
+-- local hudUIManager = require(uiFolder:WaitForChild("HUDUIManager"))
+-- hudUIManager.new(platformDetectionManager)
 
