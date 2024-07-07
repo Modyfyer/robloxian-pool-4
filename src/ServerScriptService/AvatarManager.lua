6a632acfe5b3ae45b475352ac96e297e66a78fa3
Emma <Modyfyer@users.noreply.github.com>
2024-07-06 23:29:36 -0700

    Display inventory items in menu

--- src/ServerScriptService/AvatarManager.lua
+++ src/ServerScriptService/AvatarManager.lua
@@ -4,20 +4,21 @@ Module purpose: Handles the avatar backend
 Initialized by: ServerInit
 --]]--<<---------------------------------------------------->>--
 --Services
-local DataStoreService = game:GetService("DataStoreService")
-local MarketplaceService = game:GetService("MarketplaceService")
-local Players = game:GetService("Players")
+--local MarketplaceService = game:GetService("MarketplaceService")
+--local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local ServerStorage = game:GetService("ServerStorage")
+local ServerScriptService = game:GetService("ServerScriptService")
+--local ServerStorage = game:GetService("ServerStorage")
 
 --Modules
 local ConnectionManager = require(ReplicatedStorage.ConnectionManager)
-local Event = require(ReplicatedStorage.Utils.Event)
-local ItemsData = require(ReplicatedStorage.Data.ItemsData)
+local DataTypes = require(ServerScriptService.Data.DataTypes)
+--local ItemsData = require(ReplicatedStorage.Data.ItemsData)
 
 --Declarations
 local BindableEvents: Folder = ReplicatedStorage:WaitForChild("BindableEvents")
 local RemoteEvents: Folder = ReplicatedStorage:WaitForChild("RemoteEvents")
+local RemoteFunctions: Folder = ReplicatedStorage:WaitForChild("RemoteFunctions")
 
 local AvatarManager = {}
 AvatarManager.__index = AvatarManager
@@ -30,15 +31,66 @@ function new(dataManager, purchaseManager)
 
 	-- Dependency group 0
 	self._connectionManager = ConnectionManager.new()
+	self._dataManager = dataManager
 	self._purchaseManager = purchaseManager
+
+	self.LoadAvatarEvent = RemoteEvents:WaitForChild("LoadAvatar") :: RemoteEvent
+	self.SaveAvatarEvent = RemoteEvents:WaitForChild("SaveAvatar") :: RemoteEvent
+
+	self.LoadInventoryEvent = RemoteEvents:WaitForChild("LoadInventory") :: RemoteEvent
+	self.SaveInventoryEvent = RemoteEvents:WaitForChild("SaveInventory") :: RemoteEvent
 
 	_connectHandlers(self)
 
 	return self
 end
 
-function _connectHandlers(self)
+function AvatarManager:LoadAvatar(player: Player, data: DataTypes.DataTemplate)
+	local outfit = data.currentOutfit
+	RemoteEvents:WaitForChild("LoadAvatar"):FireClient(player, outfit)
+end
 
+function AvatarManager:LoadInventory(player: Player, data: DataTypes.DataTemplate)
+	local inventory = data.inventory
+	RemoteEvents:WaitForChild("LoadInventory"):FireClient(player, inventory)
+end
+
+function _connectHandlers(self)
+	self._connectionManager:ConnectToEvent(self.SaveAvatarEvent.OnServerEvent, function(player: Player, outfit: DataTypes.Outfit)
+		_saveAvatar(self, player, outfit)
+	end)
+
+	self._connectionManager:ConnectToEvent(BindableEvents.ProfileLoaded.Event, function(player: Player, data: DataTypes.DataTemplate)
+		-- local testItem: DataTypes.InventoryItem = {
+		-- 	itemID = "dfsfdds",
+		-- 	displayName = "Hat",
+		-- 	image = "",
+		-- 	category = "hats"
+		-- }
+		-- _giveItem(self, player, testItem)
+		AvatarManager:LoadAvatar(player, data)
+		AvatarManager:LoadInventory(player, data)
+	end)
+
+	-- self._connectionManager:ConnectToEvent(RemoteFunctions.GetAvatar.OnServerInvoke, function(player: Player)
+	-- 	print()
+	-- end)
+end
+
+function _giveItem(self, player: Player, item: DataTypes.InventoryItem)
+	local inventory = self._dataManager.getKey(player, "inventory")
+	table.insert(inventory[item.category], item)
+	_saveInventory(self, player, inventory)
+end
+
+function _saveAvatar(self, player: Player, outfit: DataTypes.Outfit)
+	local saved = self._dataManager.setKey(player, "currentOutfit", outfit)
+	warn("Outfit data saved for", player.name, ":", saved)
+end
+
+function _saveInventory(self, player: Player, inventory: DataTypes.Inventory)
+	local saved = self._dataManager.setKey(player, "inventory", inventory)
+	warn("Inventory data saved for", player.name, ":", saved)
 end
 
 return {
--- src/StarterPlayer/StarterPlayerScripts/Managers/UI/AvatarUIManager/DesktopUIManager.lua
+++ src/StarterPlayer/StarterPlayerScripts/Managers/UI/AvatarUIManager/DesktopUIManager.lua
@@ -5,7 +5,7 @@ Initialized by: AvatarUIManager
 --]]--<<---------------------------------------------------->>--
 
 --Services
-local Players = game:GetService("Players")
+--local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local TweenService = game:GetService("TweenService")
 
@@ -16,7 +16,8 @@ local ConnectionManager = require(ReplicatedStorage.ConnectionManager)
 --local LocalPlayer = Players.LocalPlayer
 
 local BindableEvents: Folder = ReplicatedStorage:WaitForChild("BindableEvents")
---local RemoteEvents: Folder = ReplicatedStorage:WaitForChild("RemoteEvents")
+local Items: Folder = ReplicatedStorage:WaitForChild("Items")
+local RemoteEvents: Folder = ReplicatedStorage:WaitForChild("RemoteEvents")
 
 local menuTween: TweenInfo = TweenInfo.new(0.25, Enum.EasingStyle.Quint, Enum.EasingDirection.In)
 
@@ -29,13 +30,22 @@ function new(screenGui)
 
 	self._mainFrame = screenGui:WaitForChild("Desktop") :: Frame
 	self._connectionManager = ConnectionManager.new()
+	self._inventory = {}
+	self._outfit = {}
 
 	self._background = self._mainFrame:WaitForChild("BG") :: Frame
 
 	self._closeButton = self._background:WaitForChild("CloseButton") :: GuiButton
+	self._inventoryContainer = self._background:WaitForChild("ScrollingFrame") :: ScrollingFrame
+
+	self._slotPrefab = self._inventoryContainer:WaitForChild("Slot") :: ImageLabel
 
 	self.AvatarButtonPressed = BindableEvents:WaitForChild("AvatarButtonPressed") :: BindableEvent
 	self.AvatarMenuClosed = BindableEvents:WaitForChild("AvatarMenuClosed") :: BindableEvent
+	self.LoadAvatar = RemoteEvents:WaitForChild("LoadAvatar") :: RemoteEvent
+	self.SaveAvatar = RemoteEvents:WaitForChild("SaveAvatar") :: RemoteEvent
+	self.LoadInventory = RemoteEvents:WaitForChild("LoadInventory") :: RemoteEvent
+	self.SaveInventory = RemoteEvents:WaitForChild("SaveInventory") :: RemoteEvent
 
 	_connectHandlers(self)
 
@@ -81,6 +91,74 @@ function _connectHandlers(self)
 	self._connectionManager:ConnectToEvent(closeTween.Completed, function()
 		self.AvatarMenuClosed:Fire()
 	end)
+	self._connectionManager:ConnectToEvent(self.LoadInventory.OnClientEvent, function(inventory)
+		self._inventory = inventory
+		_clearInventoryFrame(self)
+		_createInventoryItemFrames(self)
+	end)
+	self._connectionManager:ConnectToEvent(self._inventoryContainer.UIListLayout.Changed, function(property)
+		if property ~= "AbsoluteContentSize" then
+			return
+		end
+
+		local contentSize: Vector2 = self._inventoryContainer.UIListLayout.AbsoluteContentSize
+		self._inventoryContainer.CanvasSize = UDim2.fromOffset(0, contentSize.Y)
+	end)
+end
+
+function _clearInventoryFrame(self)
+	for _, v in pairs(self._inventoryContainer:GetChildren()) do
+		if v:IsA("Frame") and v.Name ~= "Slot" then
+			v:Destroy()
+		end
+	end
+end
+
+function _createInventoryItemFrames(self)
+	local inventory = self._inventory
+	local categoriesToDisplay: {string} = {"floaties", "hats", "outfits", "pants"}
+
+	local function _createItemFrame(item, category)
+		local newSlot: ImageLabel = self._slotPrefab:Clone()
+		newSlot:SetAttribute("Category", category)
+		newSlot.ItemName.Text = item.displayName
+		newSlot.Visible = true
+		newSlot.Name = "ItemSlot"
+		newSlot.Parent = self._inventoryContainer
+	end
+
+	for category, data in pairs(inventory) do
+		if table.find(categoriesToDisplay, tostring(category)) then
+			for _, item in pairs(data) do
+				_createItemFrame(item, category)
+			end
+		end
+	end
+
+end
+
+function _loadAvatar(self)
+
+end
+
+function _saveAvatar(self)
+	local outfit = {}
+	if self._outfit ~= nil then
+		outfit = self._outfit
+	end
+	self.SaveAvatar:FireServer(outfit)
+end
+
+function _loadInventory(self)
+	_clearInventoryFrame(self)
+end
+
+function _saveInventory(self)
+	local inventory = {}
+	if self._inventory ~= nil then
+		inventory = self._inventory
+	end
+	self.SaveInventory:FireServer(inventory)
 end
 
 return {
