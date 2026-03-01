af8c91c3ec09546f2df53003aea274b2945e2c40
Emma <Modyfyer@users.noreply.github.com>
2026-03-01 13:37:47 -0800

    Server init to Knit

--- src/ServerScriptService/ServerInit.server.lua
+++ src/ServerScriptService/ServerInit.server.lua
@@ -1,31 +1,24 @@
---[[--<<---------------------------------------------------->>--
-Purpose: Initializes high level objects
-
-Initialized by: Self
---]]--<<---------------------------------------------------->>--
-
---local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local ServerScriptService = game:GetService("ServerScriptService")
 
---Dependency group 0
-local DataManager = require(ServerScriptService:WaitForChild("Data"):WaitForChild("DataManager"))
-DataManager.new()
+-- --Dependency group 0
+-- local DataManager = require(ServerScriptService:WaitForChild("Data"):WaitForChild("DataManager"))
+-- DataManager.new()
 
-repeat
-    task.wait()
-until DataManager ~= nil
+-- repeat
+--     task.wait()
+-- until DataManager ~= nil
 
---Dependency group 1
-local PurchaseManager = require(ServerScriptService:WaitForChild("Data"):WaitForChild("PurchaseManager"))
-PurchaseManager.new(DataManager)
+-- --Dependency group 1
+-- local PurchaseManager = require(ServerScriptService:WaitForChild("Data"):WaitForChild("PurchaseManager"))
+-- PurchaseManager.new(DataManager)
 
-local SettingsManager = require(ServerScriptService:WaitForChild("SettingsManager"))
-SettingsManager.new(DataManager)
+-- local SettingsManager = require(ServerScriptService:WaitForChild("SettingsManager"))
+-- SettingsManager.new(DataManager)
 
---Dependency group 2
-local AvatarManager = require(ServerScriptService:WaitForChild("AvatarManager"))
-AvatarManager.new(DataManager, PurchaseManager)
-local CabanaManager = require(ServerScriptService:WaitForChild("CabanaManager"))
-CabanaManager.new(DataManager, PurchaseManager)
-local ShopsManager = require(ServerScriptService:WaitForChild("ShopsManager"))
-ShopsManager.new(DataManager, PurchaseManager)
+-- --Dependency group 2
+-- local AvatarManager = require(ServerScriptService:WaitForChild("AvatarManager"))
+-- AvatarManager.new(DataManager, PurchaseManager)
+-- local CabanaManager = require(ServerScriptService:WaitForChild("CabanaManager"))
+-- CabanaManager.new(DataManager, PurchaseManager)
+-- local ShopsManager = require(ServerScriptService:WaitForChild("ShopsManager"))
+-- ShopsManager.new(DataManager, PurchaseManager)
