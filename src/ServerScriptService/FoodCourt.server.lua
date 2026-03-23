7b8603cbd50569f5c95e3732cb1236c2a3a5bce0
Emma <Modyfyer@users.noreply.github.com>
2026-03-23 13:37:23 -0700

    FoodOrderComponent

--- src/ServerScriptService/FoodCourt.server.lua
+++
@@ -1,6 +0,0 @@
-local orderEvent = game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvents"):WaitForChild("OrderFood")
-
-orderEvent.OnServerEvent:connect(function(player, foodItem)
-	local food = foodItem:Clone()
-	food.Parent = player.Backpack
-end)
